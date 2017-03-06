cee3767593db68bb6566fdcc61d55a73d02f88cb
Texier-Atger Lucas <ltexier10@gmail.com>
2017-03-06 02:09:53 +0100

    logger: diverses amélioraions

--- logger/logger.h
+++ logger/logger.h
@@ -6,192 +6,142 @@
 #include <errno.h>
 #include <string.h>
 
-#define RED 0
-#define GREEN 1
-#define YELLOW 2
-#define BLUE 3
-#define MAGENTA 4
-#define CYAN 5
-#define WHITE 6
-
 char *close_color = "\x1b[0m";
-char __buf[256], __buf2[256], __buf3[256];
+char __buf[1024];
 
 typedef enum {
-  Red,
-  Green,
-  Yellow,
-  Blue,
-  Magenta,
-  Cyan,
-  White,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE,
 } Color;
 
 typedef enum {
-  Success,
-  Info,
-  Debug,
-  Warning,
-  Error,
-  Fatal,
+  SUCCESS,
+  INFO,
+  DEBUG,
+  WARNING,
+  ERROR,
+  FATAL,
 } LogLevel;
 
-Color color_of_level(LogLevel level);
-char* string_of_color(Color color);
-char* string_of_level(LogLevel level);
-void perror_exit(char* msg);
-
-#define colorf(color, fmt, ...) \
-  memset(&__buf, 0, sizeof(__buf)); \
-  sprintf(__buf, "%s%s%s", \
-    string_of_color(color), fmt, close_color); \
-  fprintf(stdout, __buf, ##__VA_ARGS__);
-
-#define greenf(fmt, ...) colorf(GREEN, fmt, ##__VA_ARGS__);
-#define yellowf(fmt, ...) colorf(YELLOW, fmt, ##__VA_ARGS__);
-#define redf(fmt, ...) colorf(RED, fmt, ##__VA_ARGS__);
-#define bluef(fmt, ...) colorf(BLUE, fmt, ##__VA_ARGS__);
-#define magentaf(fmt, ...) colorf(MAGENTA, fmt, ##__VA_ARGS__);
-#define cyanf(fmt, ...) colorf(CYAN, fmt, ##__VA_ARGS__);
-#define whitef(fmt, ...) colorf(WHITE, fmt, ##__VA_ARGS__);
-
-#define error_exitf(fmt, ...) \
-  memset(&__buf2, 0, sizeof(__buf2)); \
-  sprintf(__buf2, "Error in %s:%d in function %s: %s", \
-    __FILE__, __LINE__, __func__, fmt); \
-  redf(__buf2, ##__VA_ARGS__); \
-  exit(EXIT_FAILURE);
-
-#define logf(level, fmt, ...) \
-  memset(&__buf3, 0, sizeof(__buf3)); \
-  sprintf(__buf3, "[%s] %s:%d in function %s: %s", \
-    string_of_level(level), __FILE__, __LINE__, __func__, fmt); \
-  colorf(color_of_level(level), __buf3, ##__VA_ARGS__);
-
-/* #define logf2(level, fmt, ...) \ */
-/*   __logf(__FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__); */
-
-/* void __logf(char* filename, int line, char* function, char* fmt, ...) {} */
-
-#define log_successf(fmt, ...) logf(Success, fmt, ##__VA_ARGS__);
-#define log_infof(fmt, ...) logf(Info, fmt, ##__VA_ARGS__);
-#define log_debugf(fmt, ...) logf(Debug, fmt, ##__VA_ARGS__);
-#define log_warningf(fmt, ...) logf(Warning, fmt, ##__VA_ARGS__);
-#define log_errorf(fmt, ...) logf(Error, fmt, ##__VA_ARGS__);
-#define log_fatalf(fmt, ...) logf(Fatal, fmt, ##__VA_ARGS__);
-
-void perror_exit(char* msg) {
-  char buf[256];
-
-  memset(&buf, 0, sizeof(buf));
-
-  if (strncmp(strerror(errno), "Success", 7) == 0) {
-    sprintf(buf, "%s", msg);
-  } else {
-    sprintf(buf, "%s => %s", msg, strerror(errno));
-  }
-
-  error_exitf(buf, NULL);
-}
-
 char* string_of_color(Color color) {
-  char *res = "";
+  if (color == RED)
+    return "\x1b[1;31m";
+  else if (color == GREEN)
+    return "\x1b[1;32m";
+  else if (color == YELLOW)
+    return "\x1b[1;33m";
+  else if (color == BLUE)
+    return "\x1b[1;34m";
+  else if (color == MAGENTA)
+    return "\x1b[1;35m";
+  else if (color == CYAN)
+    return "\x1b[1;36m";
 
-  switch (color) {
-  case RED:
-    res = "\x1b[1;31m";
-    break;
-
-  case GREEN:
-    res = "\x1b[1;32m";
-    break;
-
-  case YELLOW:
-    res = "\x1b[1;33m";
-    break;
-
-  case BLUE:
-    res = "\x1b[1;34m";
-    break;
-
-  case MAGENTA:
-    res = "\x1b[1;35m";
-    break;
-
-  case CYAN:
-    res = "\x1b[1;36m";
-    break;
-
-  case WHITE:
-    res = "\x1b[1;37m";
-    break;
-  }
-
-  return res;
+  // White is default
+  return "\x1b[1;37m";
 }
 
 Color color_of_level(LogLevel level) {
-  Color color = Green;
+  if (level == SUCCESS)
+    return GREEN;
+  else if (level == INFO)
+    return YELLOW;
+  else if (level == DEBUG)
+    return BLUE;
+  else if (level == WARNING)
+    return MAGENTA;
+  else if (level == ERROR)
+    return RED;
+  else if (level == FATAL)
+    return RED;
 
-  switch (level) {
-  case Success:
-    color = Green;
-    break;
-
-  case Info:
-    color = Yellow;
-    break;
-
-  case Debug:
-    color = Blue;
-    break;
-
-  case Warning:
-    color = Magenta;
-    break;
-
-  case Error:
-    color = Red;
-    break;
-
-  case Fatal:
-    color = Red;
-    break;
-  }
-
-  return color;
+  // WHITE is default
+  return WHITE;
 }
 
 char* string_of_level(LogLevel level) {
-  char *res = "";
+  if (level == SUCCESS)
+    return "SUCCESS";
+  else if (level == INFO)
+    return "INFO";
+  else if (level == DEBUG)
+    return "DEBUG";
+  else if (level == WARNING)
+    return "WARNING";
+  else if (level == ERROR)
+    return "ERROR";
 
-  switch (level) {
-  case Success:
-    res = "SUCCESS";
-    break;
-
-  case Info:
-    res = "INFO";
-    break;
-
-  case Debug:
-    res = "DEBUG";
-    break;
-
-  case Warning:
-    res = "WARNING";
-    break;
-
-  case Error:
-    res = "ERROR";
-    break;
-
-  case Fatal:
-    res = "FATAL";
-    break;
-  }
-
-  return res;
+  return  "FATAL";
 }
+
+// TODO: protect with a write mutex
+#define __fprintf(file, fmt, ...) fprintf(file, fmt, ##__VA_ARGS__)
+
+#define __format_buf(buf, fmt, ...) \
+  memset(&__buf, 0, sizeof(__buf));		\
+  sprintf(buf, fmt, ##__VA_ARGS__);		\
+
+#define fcolorf(file, color, fmt, ...)		\
+  __format_buf(__buf, "%s%s%s",			\
+    string_of_color(color), fmt, close_color)	\
+  __fprintf(file, __buf, ##__VA_ARGS__);
+
+#define colorf(color, fmt, ...) fcolorf(stdout, color, fmt, ##__VA_ARGS__)
+
+#define greenf(fmt, ...)   colorf(GREEN, fmt, ##__VA_ARGS__);
+#define yellowf(fmt, ...)  colorf(YELLOW, fmt, ##__VA_ARGS__);
+#define redf(fmt, ...)     colorf(RED, fmt, ##__VA_ARGS__);
+#define bluef(fmt, ...)    colorf(BLUE, fmt, ##__VA_ARGS__);
+#define magentaf(fmt, ...) colorf(MAGENTA, fmt, ##__VA_ARGS__);
+#define cyanf(fmt, ...)    colorf(CYAN, fmt, ##__VA_ARGS__);
+#define whitef(fmt, ...)   colorf(WHITE, fmt, ##__VA_ARGS__);
+
+#define fgreenf(file, fmt, ...)   fcolorf(file, GREEN, fmt, ##__VA_ARGS__);
+#define fyellowf(file, fmt, ...)  fcolorf(file, YELLOW, fmt, ##__VA_ARGS__);
+#define fredf(file, fmt, ...)     fcolorf(file, RED, fmt, ##__VA_ARGS__);
+#define fbluef(file, fmt, ...)    fcolorf(file, BLUE, fmt, ##__VA_ARGS__);
+#define fmagentaf(file, fmt, ...) fcolorf(file, MAGENTA, fmt, ##__VA_ARGS__);
+#define fcyanf(file, fmt, ...)    fcolorf(file, CYAN, fmt, ##__VA_ARGS__);
+#define fwhitef(file, fmt, ...)   fcolorf(file, WHITE, fmt, ##__VA_ARGS__);
+
+#define flogf(file, level, fmt, ...)					\
+  __format_buf(__buf, "%s[%s]%s %s:%d in function %s: %s",		\
+    string_of_color(color_of_level(level)), string_of_level(level),	\
+    close_color,							\
+    __FILE__, __LINE__, __func__, fmt);					\
+  __fprintf(file, __buf, ##__VA_ARGS__);
+
+#define logf(fmt, ...) flogf(stdout, fmt, ##__VA_ARGS__)
+
+#define successf(fmt, ...) logf(SUCCESS, fmt, ##__VA_ARGS__);
+#define infof(fmt, ...)	   logf(INFO, fmt, ##__VA_ARGS__);
+#define debugf(fmt, ...)   logf(DEBUG, fmt, ##__VA_ARGS__);
+#define warningf(fmt, ...) logf(WARNING, fmt, ##__VA_ARGS__);
+#define errorf(fmt, ...)   logf(ERROR, fmt, ##__VA_ARGS__);
+#define fatalf(fmt, ...)   logf(FATAL, fmt, ##__VA_ARGS__);
+
+#define fsuccessf(file, fmt, ...) flogf(file, SUCCESS, fmt, ##__VA_ARGS__);
+#define finfof(file, fmt, ...)    flogf(file, INFO, fmt, ##__VA_ARGS__);
+#define fdebugf(file, fmt, ...)   flogf(file, DEBUG, fmt, ##__VA_ARGS__);
+#define fwarningf(file, fmt, ...) flogf(file, WARNING, fmt, ##__VA_ARGS__);
+#define ferrorf(file, fmt, ...)   flogf(file, ERROR, fmt, ##__VA_ARGS__);
+#define ffatalf(file, fmt, ...)   flogf(file, FATAL, fmt, ##__VA_ARGS__);
+
+#define fperror_exitf(file, fmt, ...)						\
+  if (strncmp(strerror(errno), "Success", 7) == 0) {				\
+    fcolorf(file, RED, "Error in %s:%d in function %s: %s",			\
+      __FILE__, __LINE__, __func__, fmt, NULL);					\
+  } else {									\
+    fcolorf(file, RED, "Error %s in %s:%d in function %s: %s",			\
+      strerror(errno), __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__);	\
+  }										\
+  exit(EXIT_FAILURE);
+
+#define perror_exitf(fmt, ...) fperror_exitf(stderr, ##__VA_ARGS__)
 
 #endif
--- logger/logger_test.c
+++ logger/logger_test.c
@@ -2,39 +2,40 @@
 
 int main() {
   /* colorf */
-  colorf(RED, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  colorf(GREEN, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  colorf(YELLOW, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  colorf(BLUE, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  colorf(MAGENTA, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  colorf(CYAN, "int %d, char* %s, float %f", 42, "azerty", 3.14159);
+  colorf(RED, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  colorf(GREEN, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  colorf(YELLOW, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  colorf(BLUE, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  colorf(MAGENTA, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  colorf(CYAN, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
   colorf(WHITE, "int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
 
   /* colorf shortcuts */
-  redf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  greenf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  yellowf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  bluef("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  magentaf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  cyanf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
-  whitef("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  cyanf("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  magentaf("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  bluef("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  yellowf("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  greenf("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
+  redf("int %d, char* %s, float %f\n", 42, "azerty", 3.14159);
 
   /* logf */
-  logf(Success, "logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  logf(Info, "logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  logf(Debug, "logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  logf(Warning, "logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  logf(Error, "logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  logf(Fatal, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(SUCCESS, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(INFO, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(DEBUG, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(WARNING, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(ERROR, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  logf(FATAL, "logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
 
   /* logf shortcuts */
-  log_successf("logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  log_infof("logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  log_debugf("logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  log_warningf("logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  log_errorf("logf int %d float %f char* %s", 42, 3.14159, "qwerty");
-  log_fatalf("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  errorf("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  warningf("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  debugf("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  infof("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
+  successf("logf int %d float %f char* %s\n", 42, 3.14159, "qwerty");
 
-  /* exit */
-  error_exitf("int %d, char* %s, float %f", 42, "azerty", 3.14159);
+  /* perror_exit */
+  fperror_exitf(stdout, "perror_exit's message:\n\tint %d float %f char* %s\n",
+    42, 3.14159, "qwerty");
+
+  return 0;
 }
